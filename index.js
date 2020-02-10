ee758595db6ec2c44562caddd42e887e3091f5f8
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-02-10 18:05:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function mapToNegativize(array) {
+    return array.map(i => i * -1)
+}
+
+function mapToNoChange(array) {
+    return array.map(i => i)
+}
+
+function mapToDouble(array) {
+    return array.map(i => i * 2)
+}
+
+function mapToSquare(array) {
+    return array.map(i => i ** 2)
+}
+
+function reduceToTotal(array, start=0) {
+    return array.reduce((acc, value) =>  acc + value, start)
+}
+
+function reduceToAllTrue(array) {
+   return array.reduce((acc,val) => acc = val ? true:false)
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce((acc,val) => acc = val ? true:false)
+ }
